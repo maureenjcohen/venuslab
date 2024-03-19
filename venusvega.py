43ff5d07309a58921f29da2fe7c9491176be6982
Maureen.Cohen <maureen.cohen@open.ac.uk>
2024-03-19 15:48:03 +0000

    New submodule for analysing and plotting data from the Vega balloons. Currently have a Balloon data object to hold the datasets and some basic stats/coordinate calculations, as well as a function that compares the mean wind U, V, W wind values from a GCM simulation to the balloon data.

---
+++ venusvega.py
@@ -0,0 +1,117 @@
+""" Module for processing Vega data """
+
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+## File paths ##
+# %%
+balloon1 = '/exomars/data/analysis/volume_8/mc5526/vega_data/vg1bl_rdr.dat'
+balloon2 = '/exomars/data/analysis/volume_8/mc5526/vega_data/vg2bl_rdr.dat'
+lander2 = '/exomars/data/analysis/volume_8/mc5526/vega_data/vg2lr.dat'
+
+# %%
+class Balloon:
+    """ Holds data from a Vega balloon experiment
+        Pandas DataFrame                        """
+
+    def __init__(self, balloonpath, name):
+        self.name = name # Name of experiment, i.e. 'Vega 1' or 'Vega 2'
+
+        data = pd.read_csv(balloonpath, header=None, sep='\s+')
+
+        labels = {'Time':'s', 
+                  'Pressure':'hPa', 'Pressure flag':'int',
+                  'Temperature':'K', 'Temperature flag': 'int',
+                  'Entropy':'J/K', 'Entropy flag':'int',
+                  'Backscatter': '10^-8 /M/SR', 'Backscatter flag':'int',
+                  'Effective radius':'km', 'Radius flag':'int',
+                  'Latitude':'rad', 'Latitude flag':'int',
+                  'Longitude':'rad', 'Longitude flag':'int',
+                  'U':'m/s', 'U flag':'int',
+                  'V':'m/s', 'V flag':'int',
+                  'W_a':'m/s', 'W_a flag':'int',
+                  'W_b':'m/s', 'W_b flag':'int'}
+        # Labels data taken from vg1bl_rdr.lbl and vg2bl_rdr.lbl
+
+        self.labels = labels # Store labels dictionary in object
+        cols = labels.keys() # Extract column labels to be used in header
+        data.columns = cols # Add column names to DataFrame
+        self.data = data # Add DataFrame to object
+
+        units = labels.values() # Extract units from labels
+        self.units = units # Add units to object
+
+    def explain_flags(self):
+        print('Data quality flag for previous column.')
+        print('0 indicates no data; 1 indicates erroneous data.')
+        print('2 or 3 indicate good data, but some uncertainties.') 
+        print('4 indicates high quality.')
+
+    def coords(self):
+        """ Define coords of ballon as mean pressure level
+            and mean latitude; longitude varies with drift """
+        
+        pmean = np.mean(self.data['Pressure'])
+        latclean = self.data['Latitude']
+        latclean[latclean==0] = np.nan
+        latmean = np.rad2deg(np.mean(latclean))
+
+        self.lat = latmean # Mean latitude of balloon
+        self.plev = pmean # Mean pressure level of balloon
+
+    def calc_stats(self):
+        """ Get mean U, V, W winds from whole dataset """
+
+        wmean = np.mean(self.data['W_b'])
+        uclean = self.data['U']
+        uclean[uclean==0] = np.nan
+        umean = np.mean(uclean)
+        vclean = self.data['V']
+        vclean[vclean==0] = np.nan
+        vmean = np.mean(vclean)
+
+        stats_data = {'Field': ['U', 'V', 'W'],
+                      'Mean': [umean, vmean, wmean]}
+        stats_df = pd.DataFrame(stats_data)
+        self.stats = stats_df
+        # Create DF with mean U, V, W winds
+
+
+# %%
+def compare_stats(plobject, bobject, lat, lev=25):
+
+    """ Compare mean wind field values of a 
+        simulation output and a Vega balloon 
+        
+        Vega 1: lat = 8 deg (~model 52), lev = 580 hPa (~model 25)
+        Vega 2: lat = """
+    
+    if not hasattr(bobject, 'stats'):
+        bobject.calc_stats()
+    
+    sim_u = np.mean(plobject.data['vitu'][:,lev,lat,:])
+    sim_v = np.mean(plobject.data['vitv'][:,lev,lat,:])
+
+    omega = plobject.data['vitw'][:,lev,lat,:]
+    temp = plobject.data['temp'][:,lev,lat,:]
+    pres = plobject.data['pres'][:,lev,lat,:]
+    w = -(omega*temp*plobject.RCO2)/(pres*plobject.g)
+    sim_w = np.mean(w)
+
+    compstats = {'Field': ['U', 'V', 'W'],
+                'Mean': list(bobject.stats['Mean'].values),
+                'Sim mean': [sim_u, sim_v, sim_w]}
+    compdf = pd.DataFrame(compstats)
+    
+    print('Comparison of wind fields at approx.:') 
+    print(f'lat {plobject.lats[lat]} deg,')
+    print(f'altitude {plobject.heights[lev]} km')
+
+    print(compdf.head)
+    
+    return compdf   
+
+
+# %%
