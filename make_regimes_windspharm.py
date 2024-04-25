5130caddfd2f408afe81457574f7bc5ad6e0ac42
Maureen.Cohen <maureenjcohen@gmail.com>
2024-04-25 08:40:53 +0100

    Building Helmholtz decomp figure. Currently buggy, probably need to extract more data for higher ntrunc.

--- make_regimes_windspharm.py
+++ make_regimes_windspharm.py
@@ -7,7 +7,7 @@
 
 # Input file paths and outputs - only part of script that should be edited
 # %%
-vpcm_path = '/home/maureenjcohen/.nc'
+vpcm_path = '/home/maureenjcohen/lmd_data/aoa_surface.nc'
 # Simulation with surface age of air tracer - baseline model state
 
 # Import packages
@@ -28,3 +28,47 @@ def init_model_data(inpath):
     plobject.setup()
 
     return plobject
+
+# %%
+def helm_panels(plobject, time_slice=-2, levs=[8,15,22], qscale=1, 
+                n=3, fsize=14):
+    """ Figure with 6 sub-figures, showing the wind vectors and Helmholtz decomp
+        at 3 different altitude levels                                  """
+    
+    u = -np.copy([plobject.data['vitu'][time_slice,lev,:,:] for lev in levs])
+    v = np.copy([plobject.data['vitv'][time_slice,lev,:,:] for lev in levs])
+
+    winds = windspharm.standard.VectorWind(u, v)
+    # Create a VectorWind data object from the x and y wind cubes
+    uchi, vchi, upsi, vpsi = winds.helmholtz(truncation=21)
+
+    zonal_upsi = np.mean(upsi, axis=-1)
+    zonal_vpsi = np.mean(vpsi, axis=-1)
+
+    eddy_vpsi = vpsi - zonal_vpsi[:,:,np.newaxis]
+
+    X, Y = np.meshgrid(plobject.lons, plobject.lats)
+    fig, ax = plt.subplots(len(levs), 2, figsize=(12,12), sharex=True, sharey=True)
+
+    plot_labels = list(map(chr, range(ord('a'), ord('z')+1))) # List containing letters of the alphabet
+    for i in range(0,len(levs)):
+        alph_i = i*2 # Index x2, equivalent to skipping every other letter of the alphabet
+        lev1_winds = ax[i,0].quiver(X[::n, ::n], Y[::n, ::n], -u[i,::n,::n],
+                    v[i,::n,::n], angles='xy', scale_units='xy', scale=qscale)
+        ax[i,0].quiverkey(lev1_winds, X=0.9, Y=1.05, U=qscale*10, label='%s m/s' %str(qscale*10),
+                    labelpos='E', coordinates='axes')
+        ax[i,0].set_ylabel('Latitude / deg', fontsize=fsize)
+        if i == len(levs)-1:
+            ax[i,0].set_xlabel('Longitude /deg', fontsize=fsize)
+        ax[i,0].set_title(f'{plot_labels[alph_i]}) Horizontal wind, h={np.round(plobject.heights[levs[0]],0)} km', fontsize=fsize)
+
+        lev1_helm = ax[i,1].quiver(X[::n,::n], Y[::n,::n], eddy_upsi[i,::n,::n], eddy_vpsi[i,::n,::n],
+                    angles='xy', scale_units='xy', scale=qscale)
+        ax[i,1].quiverkey(lev1_helm, X=0.9, Y=1.05, U=qscale*10, label='%s m/s' %str(qscale*10),
+                    labelpos='E', coordinates='axes')
+        ax[i,1].set_title(f'{plot_labels[alph_i+1]}) Eddy rotational component, h={np.round(plobject.heights[levs[0]],0)} km', fontsize=fsize)
+        if i == len(levs)-1:
+            ax[i,1].set_xlabel('Longitude /deg', fontsize=fsize)
+
+    plt.show()
+# %%
