903e40ba044ca56493f6072f6689a51be42a310a
Maureen.Cohen <maureenjcohen@gmail.com>
2024-07-17 10:23:57 +0100

    Added function for long-lived tracer eddy diffusion coefficient profile to eddy_diffusion. Not yet included writing outputs to csv.

--- eddy_diffusion.py
+++ eddy_diffusion.py
@@ -2,13 +2,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import netCDF4 as nc
+import pandas as pd
 
 from venusdata import *
 
-
+# %%
+datapath = '/home/maureenjcohen/lmd_data/aoa_surface.nc'
+#datapath = '/exomars/data/analysis/volume_8/mc5526/aoa_surface.nc'
 # %%
 sim = Planet(venusdict)
-sim.load_file('/exomars/data/analysis/volume_8/mc5526/aoa_surface.nc')
+sim.load_file(datapath)
 sim.setup()
 
 # %%
@@ -17,7 +20,9 @@ def short_uniform(plobject, t_c):
         tracer with uniform chemical equilibrium    
         
     Inputs:    Planet object containing simulation data 
-               t_c is the chemical relaxation time-scale"""
+               t_c is the chemical relaxation time-scale
+               
+    Outputs:    csv file containing pressure levels, approx. altitudes, and coefficients """
 
     if not hasattr(plobject,'w_wind'):
         plobject.calc_w() # Get w in m/s
@@ -34,5 +39,30 @@ def short_uniform(plobject, t_c):
 
     k_zz = w_bar**2/(t_d + t_c)
 
+
     return k_zz
+
 # %%
+def long(plobject):
+    """ Calculate eddy diffusion coefficient profile for a long-lived tracer
+    Inputs:  Planet object containing simulation data
+    
+    Outputs:    csv file containing pressure levels, approx. altitudes, and coefficients """
+
+    if not hasattr(plobject,'w_wind'):
+        plobject.calc_w() # Get w in m/s
+
+    w_mean = np.mean(plobject.w_wind, axis=0) # Time mean of w [m/s]
+    w_squared = w_mean**2
+    w_bar = np.sum(w_squared*plobject.data['aire'][:], axis=(1,2))/plobject.area
+    # Area-weighted global mean on levels 
+
+    u_mean = np.mean(plobject.data['vitu'], axis=0) # Time mean of u
+    u_bar = np.sum(u_mean*plobject.data['aire'][:], axis=(1,2))/plobject.area 
+    # Area-weighted global mean on levels
+    t_d = plobject.radius/u_bar
+    # Dynamical transport timescale: characteristic length (radius / horizontal wind)
+
+    k_zz = w_bar*t_d
+
+
