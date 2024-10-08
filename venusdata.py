Fixing bugs in calculation of Rossby radii: wrong gas constant value and inconsistent reference pressure for theta calc.

venusdata.py
@@ -23,6 +23,7 @@ heights50_old = [0.00, 0.03, 0.12, 0.32, 0.68, 1.23, 2.03, 3.10, 4.50, 6.23, 8.3
                46.9, 49.5, 51.9, 54.1, 56.2, 58.1, 60.1, 61.9, 63.7, 65.5, 67.2,
                68.8, 70.5, 72.2, 73.8, 75.5, 77.1, 78.7, 80.2, 81.8, 83.3, 84.8,
                86.2, 87.8, 90.1, 92.9, 94.9, 101.]
+## Heights in km calculated from global area-weighted long-term mean of geopotential height
 heights50 = [0.,  0.05,  0.2,  0.4,  0.8,  1.3,  2.2,  3.3,  4.7,  6.5,  8.6,
        11.1, 14., 17.3, 20.9, 24.7, 28.5, 32.1, 35.4, 38.6, 41.6, 44.4,
        47.1, 49.7, 52.1, 54.3, 56.4, 58.4, 60.3, 62.1, 63.9, 65.6, 67.4,
@@ -144,16 +145,16 @@ class Planet:
         self.T0 = T0
         self.v0 = v0
 
-    def calc_theta(self):
+    def calc_theta(self, pref=9.2e6):
         """ Formula VPCM uses for potential temperature to account for
         specific heat capacity varying with height.
         See Lebonnois et al 2010.   """
         if self.model=='vpcm':
             if not hasattr(self, 'cp'):
                 self.calc_cp()
-            p0 = 100000
+            p0 = pref
             theta_v = (self.data['temp'][:]**self.v0 +
-                    self.v0*(self.T0**self.v0)*(np.log((p0/self.data['pres'][:]))**(self.RCO2/self.cp0)))
+                    self.v0*(self.T0**self.v0)*(np.log((p0/self.data['pres'][:])**(self.RCO2/self.cp0))))
             theta = theta_v**(1/self.v0)
             self.theta = theta
         elif self.model=='oasis':

venuspioneer.py
@@ -68,7 +68,7 @@ class Probe:
 
         data = pd.read_csv(probepath, sep=',')
         self.data = data # Add DataFrame to object
-        self.RCO2 = 287.05
+        self.RCO2 = 188.92
         self.g = 8.87 
         self.radius = 6051.3 # km
 
@@ -121,19 +121,19 @@ class Probe:
         cp0 = 1000 # J/kg/K
         T0 = 460 # K
         v0 = 0.35 # exponent
-        cp = cp0*(self.data['T(DEG K)'][:]/T0)**v0
+        cp = cp0*((self.data['T(DEG K)'][:]/T0)**v0)
         self.cp = cp
         self.cp0 = cp0
         self.T0 = T0
         self.v0 = v0
 
-    def calc_theta(self):
+    def calc_theta(self, pref=9.2e6):
         """ Formula LMDZ Venus uses for potential temperature to account for
         specific heat capacity varying with height.
         See Lebonnois et al 2010.   """
         if not hasattr(self, 'cp'):
             self.calc_cp()
-        p0 = self.data['P(BARS)'].values[-1]
+        p0 = pref*1e-5
         theta_v = (self.data['T(DEG K)'][:]**self.v0 +
                    self.v0*(self.T0**self.v0)*(np.log((p0/self.data['P(BARS)'][:])**(self.RCO2/self.cp0))))
         theta = theta_v**(1/self.v0)

venusrossby.py
@@ -14,7 +14,7 @@ def omega_profile(plobject, hrange=(0,-1), trange=(0,-1),
     zwind = np.mean(plobject.data['vitu'][trange[0]:trange[1],hrange[0]:hrange[1],:,:],0)
     if gmean==True:
         grid_areas = plobject.data['aire'][:]
-        zmean = np.sum(zwind*grid_areas[np.newaxis,:,:], axis=(1,2))/(plobject.area)
+        zmean = np.sum(zwind*grid_areas, axis=(1,2))/(plobject.area)
     # Area-weighted spatial and time mean of zonal wind
     else:
         zmean = np.mean(zwind,axis=-1)
@@ -54,7 +54,7 @@ def bv_freq(plobject, hrange=(0,-1), trange=(0,-1),
     th_mean = np.mean(plobject.theta[trange[0]:trange[1],hrange[0]:hrange[1],:,:], axis=0)
     if gmean==True:
         grid_areas = plobject.data['aire'][:]
-        th_prof = np.sum(th_mean*grid_areas[np.newaxis,:,:], axis=(1,2))/(plobject.area)
+        th_prof = np.sum(th_mean*grid_areas, axis=(1,2))/(plobject.area)
         th_dz = np.gradient(th_prof)/np.gradient(plobject.heights[hrange[0]:hrange[1]]*1000)
     else:
         th_prof = np.mean(th_mean, axis=-1)
@@ -95,12 +95,16 @@ def coriolis(plobject, gmean=True, lat=80, hrange=(0,-1), trange=(0,-1)):
 def scaleheight(plobject, hrange=(0,-1), trange=(0,-1),
                 gmean=True, lat=80):
 
-    """ Approximate velocity of a gravity wave      """
+    """ Scale height of atmosphere as a function of altitude  
+        kT/mg where k is Boltzmann's constant, T is temp, m is average mass of atoms (CO2) in kg,
+        and g is gravity
+        This is equivalent to RT/Mg, where R is universal gas constant (8.314 Nm/molK), T
+        is temp, M is molar mass (kg/mol), and g is gravity"""
 
     mean_temp = np.mean(plobject.data['temp'][trange[0]:trange[1],hrange[0]:hrange[1],:,:], axis=0)
     if gmean==True:
         grid_areas = plobject.data['aire'][:]
-        global_mean = np.sum(mean_temp*grid_areas[np.newaxis,:,:], axis=(1,2))/(plobject.area)
+        global_mean = np.sum(mean_temp*grid_areas, axis=(1,2))/(plobject.area)
     else:
         global_mean = np.mean(mean_temp, axis=-1)
         global_mean = global_mean[:,lat]